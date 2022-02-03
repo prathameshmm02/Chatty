c2847facd6822b470c5a7a6a1be695fecac49d84
Prathamesh More <prathameshmm02@gmail.com>
2022-02-03 22:19:57 +0530

    Added firebase and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import './App.css';
-import SignIn from './Components/SignIn';
+import "./App.css";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
+import { initializeApp } from "firebase/app";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAabTWUwgcTkOXeWM-NdrxXpW2YV-4AKlA",
+  authDomain: "chatty-8ac08.firebaseapp.com",
+  projectId: "chatty-8ac08",
+  storageBucket: "chatty-8ac08.appspot.com",
+  messagingSenderId: "313924004391",
+  appId: "1:313924004391:web:c030bf38ff3b8886073c02",
+  measurementId: "G-HKLXBBJHPP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
-  return <SignIn/>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={ <SignIn/> } />
+        <Route path="/sign-up" element={ <SignUp/> } />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
