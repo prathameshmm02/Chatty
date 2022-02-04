c6d5ed68f5be4bd5d5bfcdfda9a5e98f66da1f88
Prathamesh More <prathameshmm02@gmail.com>
2022-02-04 21:40:59 +0530

    Better Start-up page

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -2,6 +2,7 @@ import React from "react";
 import chatImage from "../chatImage.svg";
 import google from "../Google.svg";
 import { useState } from "react";
+
 import {
   getAuth,
   signInWithPopup,
@@ -17,12 +18,6 @@ const signInWithEmailPass = () => {
   signInWithEmailAndPassword(auth, email, password);
 };
 
-const signInWithGoogle = () => {
-  const provider = new GoogleAuthProvider();
-  const auth = getAuth();
-  signInWithPopup(auth, provider);
-};
-
 const signUpWithEmailPass = () => {
   const auth = getAuth();
   const email = document.getElementById("email").value;
@@ -30,81 +25,109 @@ const signUpWithEmailPass = () => {
   createUserWithEmailAndPassword(auth, email, password);
 };
 
+const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  signInWithPopup(auth, provider);
+};
+
 export default function Start() {
+  const [isLoginScreen, setLoginScreen] = useState(true);
+
   return (
     <div className="container">
       <div>
         <img src={chatImage} className="chat-image" alt="logo" />
       </div>
-      <SignIn />
+      {isLoginScreen ? <SignIn/> : <SignUp/>}
     </div>
   );
-}
 
-function SignIn() {
-  return (
-    <div className="content">
-      <h1 className="title">Chatty</h1>
-      <input
-        id="email"
-        type="text"
-        className="form-control"
-        placeholder="E-Mail"
-      />
-      <input
-        id="password"
-        type="password"
-        className="form-control"
-        placeholder="Password"
-      />
-      <button
-        type="button"
-        className="btn btn-primary login"
-        onClick={signInWithEmailPass}
-      >
-        Login
-      </button>
-      <button type="button" className="btn btn-link">
-        Create account
-      </button>
-      <button
-        type="button"
-        className="btn btn-primary google-signin"
-        onClick={signInWithGoogle}
-      >
-        <img className="google-logo" src={google} alt="" />
-        Sign in with Google
-      </button>
-    </div>
-  );
-}
+  function SignIn() {
+    return (
+      <div className="content">
+        <h1 className="title">Chatty</h1>
+        <input
+          id="email"
+          type="text"
+          className="form-control"
+          placeholder="E-Mail"
+        />
+        <input
+          id="password"
+          type="password"
+          className="form-control"
+          placeholder="Password"
+        />
+        <button
+          type="button"
+          className="btn btn-primary submit"
+          onClick={signInWithEmailPass}
+        >
+          Login
+        </button>
+        <button
+          type="button"
+          className="btn btn-link"
+          onClick={(e) => {
+            setLoginScreen(false);
+          }}
+        >
+          Create account
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={signInWithGoogle}
+        >
+          <img className="google-logo" src={google} alt="" />
+          Sign in with Google
+        </button>
+      </div>
+    );
+  }
 
-function SignUp() {
-  return (
-    <div className="content">
-      <h1 className="title">Chatty</h1>
-      <input
-        id="email"
-        type="text"
-        className="form-control"
-        placeholder="E-Mail"
-      />
-      <input
-        id="password"
-        type="password"
-        className="form-control"
-        placeholder="Password"
-      />
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={signUpWithEmailPass}
-      >
-        Register
-      </button>
-      <button type="button" className="btn btn-link">
-        Create account
-      </button>
-    </div>
-  );
+  function SignUp() {
+    return (
+      <div className="content">
+        <h1 className="title">Chatty</h1>
+        <input
+          id="email"
+          type="text"
+          className="form-control"
+          placeholder="E-Mail"
+        />
+        <input
+          id="password"
+          type="password"
+          className="form-control"
+          placeholder="Password"
+        />
+        <button
+          type="button"
+          className="btn btn-primary submit"
+          onClick={signUpWithEmailPass}
+        >
+          Register
+        </button>
+        <button
+          type="button"
+          className="btn btn-link"
+          onClick={(e) => {
+            setLoginScreen(true);
+          }}
+        >
+          Login instead
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={signInWithGoogle}
+        >
+          <img className="google-logo" src={google} alt="" />
+          Register with Google
+        </button>
+      </div>
+    );
+  }
 }
