d27b016498a5c03112197aa4e625995a8b5a83a4
Prathamesh More <prathameshmm02@gmail.com>
2022-03-14 19:54:40 +0530

    Added TabLayout to switch between group and personal chats

--- src/Components/ChatList.js
+++ src/Components/ChatList.js
@@ -1,6 +1,6 @@
 import { getAuth } from "firebase/auth";
 import { collection, getFirestore, query, where } from "firebase/firestore";
-import React from "react";
+import React, { useState } from "react";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import ChatListItem from "./ChatListItem";
 import NewChat from "./NewChat";
@@ -22,8 +22,10 @@ export default function ChatList({ setChatID }) {
   const signOut = () => {
     getAuth().signOut();
   };
+
+  const [currentTab, setCurrentTab] = useState(0);
   return (
-    <div className="chatlist-container">
+    <div className="chatlist-container flex flex-col h-screen">
       <div className="flex flex-row justify-between bg-slate-400 p-2">
         <h3>Chatty</h3>
         <button onClick={signOut}>
@@ -32,7 +34,34 @@ export default function ChatList({ setChatID }) {
           </span>
         </button>
       </div>
-      <div className="overflow-y-scroll">
+      <div>
+        <div className="flex flex-row cursor-pointer text-center ">
+          <h6
+            className="w-1/2 py-3 m-0"
+            onClick={() => {
+              setCurrentTab(0);
+            }}
+          >
+            Groups
+          </h6>
+          <h6
+            className="w-1/2 py-3 m-0"
+            onClick={() => {
+              setCurrentTab(1);
+            }}
+          >
+            Personal
+          </h6>
+        </div>
+        <div
+          className={
+            "h-1 bg-accent w-1/2 rounded-full transition ease-out duration-300 " +
+            (currentTab === 0 ? "translate-x-0" : "translate-x-full")
+          }
+        ></div>
+      </div>
+
+      <div className="overflow-y-scroll flex-grow flex-shrink">
         {chats &&
           chats.map((chat) => (
             <ChatListItem key={chat.id} chat={chat} setChatID={setChatID} />
