81819ba9ccc0e22ce3fdef9e2ce631f168cc303a
Prathamesh More <prathameshmm02@gmail.com>
2022-03-17 17:45:52 +0530

    Added framer-motion

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { motion } from 'framer-motion';
 
 export default function Modal({ selectedImg, setSelectedImg }) {
+  const handleClick = (e) => {
+    if (e.target.classList.contains('backdrop')) {
+      setSelectedImg(null);
+    }
+  }
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen bg-black/60" onClick={() => setSelectedImg(null)}>
-      <img className="max-w-[60%] max-h-[80%] my-16 mx-auto border-4 border-white shadow-lg" src={selectedImg} alt="Enlarged Pic" />
-    </div>
+    <motion.div className="backdrop fixed top-0 left-0 w-screen h-screen bg-black/60" onClick={handleClick} initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}>
+      <motion.img className="max-w-[60%] max-h-[80%] my-16 mx-auto border-4 border-white shadow-lg" src={selectedImg} alt="Enlarged Pic"  initial={{ y: "-100vh" }}
+      animate={{ y: 0 }}/>
+    </motion.div>
   );
 }
